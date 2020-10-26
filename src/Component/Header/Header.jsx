78f733eee6b139d996b6c666a434cc80063896e2
chris097 <chrisfidel.international@gmail.com>
2020-10-26 21:26:19 +0100

    upload folder file

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Header.css";
+import "../Utils/index.css"
+import searchFormIcon from "../Header/Icons/search-icon.svg";
+import addBookIcon from "../Header/Icons/add-book-icon.svg";
+
+const initialState = []
+
+const Header = () =>{
+    function displayAddBook () {
+        console.log("something was clicked...")
+    }
+
+    return (
+        <div className="container">
+            <div className="header">
+                <div className="logo">LibraShelter</div>
+                <div className="searchForm">
+                    <img src={searchFormIcon} alt="search-icon"/>
+                   <input type="search" id="search" placeholder="Search book"/>
+                </div>
+                <button 
+                className="add-book"
+                onClick={displayAddBook}
+                >
+                    <>
+                    <img src={addBookIcon} alt="Add-book-icon"/>
+                    <span>Add Book</span>
+                    </>
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css"
 import Header from "./Component/Header/Header"
 
 ReactDOM.render(
