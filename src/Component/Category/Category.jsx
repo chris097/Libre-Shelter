00716dc1d2f4b4ae7cda2d98cfb0f0f9d8bf216f
chris097 <chrisfidel.international@gmail.com>
2020-11-13 07:26:07 +0100

    add hambuger menu

--- src/Component/Category/Category.jsx
+++ src/Component/Category/Category.jsx
@@ -7,11 +7,16 @@ import FilterModal from "../Modal/Filters/Filter";
 import DisplayOption from "../Modal/Option/Option";
 import openMenu from "./Icons/menu.png";
 import dotIcon from "./Icons/dot-icon.svg";
+import closeMenu from "./Icons/close.png";
 // import DisplayOption from "../Modal/Option";
 // import FilterModal from "../Modal/Filter"
 
+const engine = (name, func) => {
+    return(
+        document.querySelector(name).style.display=func
+    )
+}
 
- 
 
 const Category = () => {
     const [openFilterModal, setOpenFilterModal] = useState(false);
@@ -28,16 +33,40 @@ const Category = () => {
         setOpenFilterModal(" ")
     }
 
+    const menuOpen = () => {
+        engine('.sidebar', 'block')
+        engine('.open-menu', 'none')
+        engine('.close-menu', 'block')
+        const div = document.querySelector('.close-menu  > img')
+        div.style.display = 'block'
+        console.log(div)
+    }
+
+
+    const menuClose = () => {
+        engine('.sidebar', 'none')
+        engine('.close-menu', 'none')
+        const div = document.querySelector('.open-menu')
+        div.style.display = 'block'
+        console.log(div)
+    }
+
     return (
         <>
         <div className="category">
             <div className="container">
-            {/* media */}
+            {/* mobile */}
             <div 
                 className="open-menu"  
-                onClick={() => console.log('something was clicked...')}          >
+                onClick={menuOpen}>
                 <img src={openMenu} alt=""/>
              </div>
+             <div className="close-menu"
+             onClick={menuClose}
+             >
+                 <img src={closeMenu} alt=""/>
+             </div>
+             {/* end of mobile */}
                 <div className="category-content">
                     <div className="fsz-20 pad-top-5 mobile-x">Category</div>
                     <div className="fiction">
