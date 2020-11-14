b97a29b9c115f7adffcd72b883143b5f44617736
chris097 <chrisfidel.international@gmail.com>
2020-11-14 10:26:09 +0100

    fix bug

--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -9,8 +9,16 @@ const SideBar = () => {
             className="sidebar"
             >
                 <ul className="sidebar-container">
-                    <li>A curated list of every book ever written</li>
+                    <p>A curated list of every book ever written</p>
                     <li>Fa Fantacy</li>
+                    <li>Ad Adventure</li>
+                    <li>Ro Romance</li>
+                    <li>Co Contemporary</li>
+                    <li>Dy Dystopian</li>
+                    <li>My Mystery</li>
+                    <li>Ho Horror</li>
+                    <li>The Thriller</li>
+                    <li>Pa Paranormal</li>
                 </ul>      
             </div>
             
